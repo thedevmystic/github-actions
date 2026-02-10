7e64bafd2cbdc4c577325be8c80b2df73474bf99
Surya! <thedevmystic@gmail.com>
2026-02-10 22:42:46 +0530

    feat: add cpp compatibility check action

---
+++ dummy_cpp_project/include/test_dummy.hpp
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2026-present Suryansh Singh
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * @path [ROOT]/dummy_cpp_project/include/test_dummy.hpp
+ * @file test_dummy.hpp
+ * @description Dummy test header file for testing the workflows.
+ *
+ * @author thedevmystic (Surya)
+ * @copyright 2026-present Suryansh Singh Apache-2.0 License
+ *
+ * SPDX-FileCopyrightText: 2026-present Suryansh Singh
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef DUMMY_CPP_PROJECT_INCLUDE_TEST_DUMMY_HPP_
+#define DUMMY_CPP_PROJECT_INCLUDE_TEST_DUMMY_HPP_
+
+int return_forty_two();
+
+bool return_true();
+
+bool return_false();
+
+#endif  // DUMMY_CPP_PROJECT_INCLUDE_TEST_DUMMY_HPP_
