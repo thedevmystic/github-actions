264dde3498d427ed2164c29b3166425f582e7c8f
Surya <thedevmystic@gmail.com>
2026-02-10 21:34:08 +0530

    fix: fix errors

--- dummy_cpp_project/tests/test_main.cpp
+++ dummy_cpp_project/tests/test_main.cpp
@@ -1,4 +1,29 @@
-// Dummy google tests
+/**
+ * Copyright 2025-present Suryansh Singh
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * @path [ROOT]/dummy_cpp_project/tests/test_main.cpp
+ * @file test_main.cpp
+ * @description Dummy test file.
+ *
+ * @author thedevmystic (Surya)
+ * @copyright 2025-present Suryansh Singh Apache-2.0 License
+ *
+ * SPDX-FileCopyrightText: 2025-present Suryansh Singh
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include <gtest/gtest.h>
 #include <test_dummy.hpp>
